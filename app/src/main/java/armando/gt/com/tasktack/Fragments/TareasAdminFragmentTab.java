6bbec3cca1ad4f5f1c92915d122e2139365324c6
ArmandoS98 <armandosantosec@gmail.com>
2018-05-28 13:36:21 -0600

    Implementacion de navegacion

--- app/src/main/java/armando/gt/com/tasktack/Fragments/TareasAdminFragmentTab.java
+++ app/src/main/java/armando/gt/com/tasktack/Fragments/TareasAdminFragmentTab.java
@@ -1,16 +1,31 @@
 package armando.gt.com.tasktack.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+
+import armando.gt.com.tasktack.Actividad.NuevaTareaActivity;
+import armando.gt.com.tasktack.Adaptadores.AdaptadorTareasAdmin;
+import armando.gt.com.tasktack.Entidad.Tasrea;
 import armando.gt.com.tasktack.R;
 
 
 public class TareasAdminFragmentTab extends Fragment {
+    private static final String TAG = "TareasAdminFragmentTab";
+
+    //Widgets
+    private RecyclerView mRecyclerViewTareasAdmin;
+    private ArrayList<Tasrea> mTasreas;
 
     public TareasAdminFragmentTab() {
         // Required empty public constructor
@@ -21,8 +36,47 @@ public class TareasAdminFragmentTab extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_tareas_admin_fragment_tab, container, false);
+        initRecycler(view);
+
+        FloatingActionButton mFloatingActionButton = view.findViewById(R.id.fab);
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), NuevaTareaActivity.class);
+                getContext().startActivity(intent);
+            }
+        });
 
         return view;
+    }
+
+    private void initRecycler(View view) {
+        mTasreas = new ArrayList<>();
+        mRecyclerViewTareasAdmin = view.findViewById(R.id.recyclerview_tareas_admin);
+        mRecyclerViewTareasAdmin.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerViewTareasAdmin.setHasFixedSize(true);
+
+        llenarDatos();
+
+        AdaptadorTareasAdmin adaptador = new AdaptadorTareasAdmin(getContext(),mTasreas);
+        mRecyclerViewTareasAdmin.setAdapter(adaptador);
+
+        adaptador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Hola que hace!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void llenarDatos() {
+        mTasreas.add(new Tasrea("Trabajo Accion","Armando Santos","http://ceplatam.com/img/blog/post/9.jpg","Primer proyecto","35%"));
+        mTasreas.add(new Tasrea("Seminario","Steve Perez","http://ceplatam.com/img/blog/post/9.jpg","Primer proyecto","55%"));
+        mTasreas.add(new Tasrea("Tesis","Daniel Blanco","http://ceplatam.com/img/blog/post/9.jpg","Primer proyecto","88%"));
+        mTasreas.add(new Tasrea("Drawable","Erick Ruiz","http://ceplatam.com/img/blog/post/9.jpg","Primer proyecto","96%"));
+        mTasreas.add(new Tasrea("Task Track","Arnoldo Chua","http://ceplatam.com/img/blog/post/9.jpg","Primer proyecto","85%"));
+        mTasreas.add(new Tasrea("Meet Guate","Mario Hugo","http://ceplatam.com/img/blog/post/9.jpg","Primer proyecto","32"));
+        mTasreas.add(new Tasrea("Root","Pedro Julio","http://ceplatam.com/img/blog/post/9.jpg","Primer proyecto","44%"));
     }
 
 }
--- app/src/main/java/armando/gt/com/tasktack/Fragments/TareasPorHacerFragmentTab.java
+++ app/src/main/java/armando/gt/com/tasktack/Fragments/TareasPorHacerFragmentTab.java
@@ -3,13 +3,25 @@ package armando.gt.com.tasktack.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+
+import armando.gt.com.tasktack.Adaptadores.AdaptadorTareasAdmin;
+import armando.gt.com.tasktack.Adaptadores.AdaptadorTareasMias;
+import armando.gt.com.tasktack.Entidad.Tasrea;
 import armando.gt.com.tasktack.R;
 
 public class TareasPorHacerFragmentTab extends Fragment {
+    private static final String TAG = "TareasPorHacerFragmentT";
+    //Widgets
+    private RecyclerView mRecyclerViewTareasMias;
+    private ArrayList<Tasrea> mTasreas;
 
     public TareasPorHacerFragmentTab() {
         // Required empty public constructor
@@ -20,8 +32,36 @@ public class TareasPorHacerFragmentTab extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_tareas_por_hacer, container, false);
-
+        initRecycler(view);
         return view;
     }
 
+    private void initRecycler(View view) {
+        mTasreas = new ArrayList<>();
+        mRecyclerViewTareasMias = view.findViewById(R.id.recyclerview_tareas_mias);
+        mRecyclerViewTareasMias.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerViewTareasMias.setHasFixedSize(true);
+
+        llenarDatos();
+
+        AdaptadorTareasMias adaptador = new AdaptadorTareasMias(getContext(),mTasreas);
+        mRecyclerViewTareasMias.setAdapter(adaptador);
+
+        adaptador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Hola que hace!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void llenarDatos() {
+        mTasreas.add(new Tasrea("Trabajo Accion","Armando Santos","http://ceplatam.com/img/blog/post/9.jpg","Primer proyecto","35%"));
+        mTasreas.add(new Tasrea("Seminario","Steve Perez","http://ceplatam.com/img/blog/post/9.jpg","Primer proyecto","55%"));
+        mTasreas.add(new Tasrea("Tesis","Daniel Blanco","http://ceplatam.com/img/blog/post/9.jpg","Primer proyecto","88%"));
+        mTasreas.add(new Tasrea("Drawable","Erick Ruiz","http://ceplatam.com/img/blog/post/9.jpg","Primer proyecto","96%"));
+        mTasreas.add(new Tasrea("Task Track","Arnoldo Chua","http://ceplatam.com/img/blog/post/9.jpg","Primer proyecto","85%"));
+        mTasreas.add(new Tasrea("Meet Guate","Mario Hugo","http://ceplatam.com/img/blog/post/9.jpg","Primer proyecto","32"));
+        mTasreas.add(new Tasrea("Root","Pedro Julio","http://ceplatam.com/img/blog/post/9.jpg","Primer proyecto","44%"));
+    }
 }
